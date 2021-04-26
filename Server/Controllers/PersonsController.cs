004b2bb43774603ad291548a0724b4acb56e3406
spolyakov <spolyakov@kostasoft.spb.ru>
2021-04-26 14:14:50 +0300

    Добавьте файлы проекта.

---
+++ Server/Controllers/PersonsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PatientsWcf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace DoctorAppWeb.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonsController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<PersonDto> Get()
+        {
+            InpatientDoctorClient inpatientDoctorClient = new InpatientDoctorClient();
+            return inpatientDoctorClient.GetPersonsAsync().Result.ToArray();
+        }
+    }
+}
