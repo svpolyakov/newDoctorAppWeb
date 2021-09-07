22728fa7a4ba3176f1a3506631e4d1a849fb0b23
Kirill <k.one.o.two@gmail.com>
2021-09-07 14:56:13 +0300

    + working pager + sorting

--- Client/Shared/Colors.cs
+++ Client/Shared/Colors.cs
@@ -8,6 +8,7 @@ namespace DoctorAppWeb.Client.Shared
     public static class Colors
     {
         public static string Blue { get { return "#3F51B5"; } }
+        public static string LightBlue { get { return "#F0F1F9"; } }
         public static string Green { get { return "#40B0A6"; } }
         public static string LightGreen { get { return "#EBF7F6"; } }
         public static string Gray { get { return "#757575"; } }
