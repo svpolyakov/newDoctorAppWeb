9d70c8598fada2eaf81cbbae687f02ae7ef03c03
Kirill <k.one.o.two@gmail.com>
2021-09-15 19:30:47 +0300

    drop block
    + finished drop block
    +apptitle in the middle
    + pale icons

---
+++ Client/CustomSettings/CustomizationItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoctorAppWeb.Client.CustomSettings
+{
+    public class CustomizationItem
+    {
+        public string ID { get; set; }
+        public string Text { get; set; }
+        public int type { get; set; }
+        public override string ToString()
+        {
+            return Text;
+        }
+    }
+}
