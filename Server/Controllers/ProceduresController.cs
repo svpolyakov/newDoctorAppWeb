74c8bd03e0e56eb118bd498b8c4cf60261ab44a5
spolyakov <spolyakov@kostasoft.spb.ru>
2021-11-02 16:46:53 +0300

    Процедуры

---
+++ Server/Controllers/ProceduresController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PatientsWcf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoctorAppWeb.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProceduresController : ControllerBase
+    {
+        [HttpGet("Procedures")]
+        public async Task<List<ProcedureResultDto>> GetProceduresAsync([FromHeader] Guid personId, [FromHeader] Guid hcsId, [FromHeader] SrvType srvType)
+        {
+            return await new InpatientDoctorClient().GetProceduresAsync(personId, hcsId, srvType);
+        }
+    }
+}
