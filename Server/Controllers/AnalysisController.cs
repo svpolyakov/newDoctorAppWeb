7ea576e665e3ee7555acca0c73fed8f6a9054420
spolyakov <spolyakov@kostasoft.spb.ru>
2021-11-08 18:46:21 +0300

    Добавлены анализы

---
+++ Server/Controllers/AnalysisController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PatientsWcf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoctorAppWeb.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AnalysisController : ControllerBase
+    {
+        [HttpGet("Analysis")]
+        public async Task<List<LabTestDto>> GetAnalysisAsync([FromHeader] Guid personId, [FromHeader] Guid hcsId, [FromHeader] SrvType srvType)
+        {
+            return await new InpatientDoctorClient().GetLabTestsAsync(personId, hcsId, srvType);
+        }
+    }
+}
