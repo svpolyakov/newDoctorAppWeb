07bd6e5718a877fa06c40b5f2985a4007e7ce3a5
spolyakov <spolyakov@kostasoft.spb.ru>
2021-11-12 18:28:49 +0300

    Выпадалка

---
+++ Server/Controllers/InfoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PatientsWcf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoctorAppWeb.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InfoController : ControllerBase
+    {
+        [HttpGet("MedDocList")]
+        public async Task<List<ServiceMedDocDto>> GetMedDocListAsync([FromHeader]Guid personId, [FromHeader]Guid hcsId)
+        {
+            return await new InpatientDoctorClient().GetMedDocListAsync(personId, hcsId);
+        }
+
+        [HttpGet("MedDoc")]
+        public async Task<MedDocDto> GetMedDocAsync([FromHeader] Guid epicrisisVersionId)
+        {
+            return await new InpatientDoctorClient().GetMedDocAsync(epicrisisVersionId);
+        }
+    }
+}
