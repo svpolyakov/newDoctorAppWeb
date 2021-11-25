b09169f5d19ac217ecc18dbb257626b41bf6ca7b
Kirill Sergeev <kirill.sergeev@introduct.tech>
2021-11-25 20:01:38 +0300

    empty state messages

---
+++ Client/Shared/EmptyStateMessages.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoctorAppWeb.Client.Shared
+{
+    static class EmptyStateMessages
+    {
+        public const string MyPatients = "У текущего пользователя нет пациентов";
+        public const string MyDepPatients = "Нет поступивших пациентов";
+        public const string EmergencyPatients = "Нет пациентов в приемном";
+        public const string AllPatients = "Пациенты отсутствуют";
+
+        public const string Diagnosis = "Диагнозы отсутствуют";
+        public const string Indications = "Показатели отсутствуют";
+        public const string Analysis = "Анализы отсутствуют";
+        public const string Meds = "Медикаменты отсутствуют";
+        public const string Research = "Исследования отсутствуют";
+        public const string Consult = "Консультации отсутствуют";
+        public const string Visit = "Визиты отсутствуют";
+        public const string Procedure = "Процедуры отсутствуют";
+        public const string Surgery = "Операции отсутствуют";
+    }
+}
