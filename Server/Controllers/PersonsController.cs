5edd84aab21a8c31e113e51981f297cc6b9d3b46
spolyakov <spolyakov@kostasoft.spb.ru>
2022-01-19 17:51:38 +0300

    Функции для пагинации

--- Server/Controllers/PersonsController.cs
+++ Server/Controllers/PersonsController.cs
@@ -24,16 +24,24 @@ namespace DoctorAppWeb.Server.Controllers
         public async Task<IEnumerable<PatientDto>> GetAsync()
         {
             int parseResult;
+            Guid DepartmentId;
+            Guid DoctorId;
             int.TryParse(Request.Headers["FilterType"], out parseResult);
             string login = Request.Headers["UserLogin"].ToString();
             if(parseResult != 0 && Enum.IsDefined(typeof(FilterPersonTypeDto), parseResult))
             {
+                PersonQueryParamsDto personQueryParams = new PersonQueryParamsDto ();
+                personQueryParams.PersonQueryType = (FilterPersonTypeDto)parseResult;
+                personQueryParams.Page = new PageParamDto { OffSet = int.Parse(Request.Headers["PageOffSet"].ToString()), Size = int.Parse(Request.Headers["PageSize"]) };
+                personQueryParams.SearchCondition = new SearchParamDto { DepartmentId = Guid.TryParse(Request.Headers["SearchDepartmentId"].ToString(), out DepartmentId) ? DepartmentId : Guid.Empty,
+                    DoctorId = Guid.TryParse(Request.Headers["SearchDoctorId"].ToString(), out DoctorId) ? DoctorId : Guid.Empty, FIOCardNumber = Request.Headers["SearchFIOCardNumber"].ToString() };
+
                 InpatientDoctorClient inpatientDoctorClient = new InpatientDoctorClient();
                 inpatientDoctorClient.Endpoint.Binding.ReceiveTimeout = new TimeSpan(0, 10, 0);
                 inpatientDoctorClient.Endpoint.Binding.OpenTimeout = new TimeSpan(0, 10, 0);
                 inpatientDoctorClient.Endpoint.Binding.SendTimeout = new TimeSpan(0, 10, 0);
                 inpatientDoctorClient.Endpoint.Binding.CloseTimeout = new TimeSpan(0, 10, 0);
-                return await inpatientDoctorClient.GetPatientsAsync( new PersonQueryParamsDto { PersonQueryType = (FilterPersonTypeDto)parseResult } , login);
+                return await inpatientDoctorClient.GetPatientsAsync( personQueryParams , login);
             }
             return null;
         }
